1463336f9cd93a7df4e370a0570558339c43f7c1
Chekoo <chekochn@gmail.com>
2018-05-24 23:50:00 +0800

    profile

--- django_auth/users/models.py
+++ django_auth/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 # Create your models here.
 
 class User(AbstractUser):
@@ -8,3 +9,8 @@ class User(AbstractUser):
 
     class Meta(AbstractUser.Meta):
         pass
+
+# 两张表通过一对一的关系关联,要查询某个用户的 Profile 时，需要执行额外的跨表查询操作
+class Profile(models.Model):
+    nickname = models.CharField(max_length=50, blank=True)
+    user = models.OneToOneField(User)
